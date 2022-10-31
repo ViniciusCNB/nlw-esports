5d56729bd78199f3d39f79a9220b3637a94783b2
Vinícius <viniborg25@gmail.com>
2022-10-31 15:37:30 -0300

    Adicionando e configurando o tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,11 @@
+import "./styles/main.css"
 
-function App() {
-  return <h1>Hello World!</h1>
+const App = () => {
+  return (
+    <div className="w-8 h-8 bg-violet-500">
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
 
 export default App
